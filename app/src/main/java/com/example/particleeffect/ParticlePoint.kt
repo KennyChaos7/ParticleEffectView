9c398d2bdcd91df449ff6b7ff03cb89c4f75a0e9
KennyChaos7 <kennychaos7@gmail.com>
2021-03-02 18:11:47 +0800

    已经基本画出圆形, 还需要研究扩散算法

--- app/src/main/java/com/example/particleeffect/ParticlePoint.kt
+++ app/src/main/java/com/example/particleeffect/ParticlePoint.kt
@@ -6,7 +6,10 @@ data class ParticlePoint(
         val maxX: Float,
         val maxY: Float,
         var speed: Float,
-        //  1: 往下
-        //  2: 往上
-        var animState: Int
+        //  位于第几象限
+        //  1: 往左上 x- y-
+        //  2: 往右上 x+ y-
+        //  3: 往左下 x- y+
+        //  4: 往右下 x+ y+
+        var state: Int
 )
