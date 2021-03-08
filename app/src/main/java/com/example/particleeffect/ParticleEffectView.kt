cd14a1428a40e4d1158989b1572a7cb9bc6c0726
KennyChaos7 <kennychaos7@gmail.com>
2021-03-08 17:48:24 +0800

    1

--- app/src/main/java/com/example/particleeffect/ParticleEffectView.kt
+++ app/src/main/java/com/example/particleeffect/ParticleEffectView.kt
@@ -8,26 +8,27 @@ import android.util.Log
 import android.view.View
 import android.view.animation.LinearInterpolator
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class ParticleEffectView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
     private val pointCount = 500
-    private var maxWidth = 0f
-    private var maxHeight = 0f
-
     private var pointPaint: Paint = Paint()
     private val ppList: MutableList<ParticlePoint> = mutableListOf()
     private var anim: ValueAnimator
-    private val circlePath = Path()
+    private val insideCirclePath = Path()
+    private val outsideCirclePath = Path()
 
     init {
         pointPaint.color = Color.WHITE
         pointPaint.strokeWidth = 2f
 
         anim = ValueAnimator.ofFloat(0f, 1f)
-        anim.duration = 1000
+        anim.duration = 5000
         anim.repeatCount = -1
         anim.interpolator = LinearInterpolator()
         anim.addUpdateListener { va ->
@@ -36,40 +37,8 @@ class ParticleEffectView @JvmOverloads constructor(
                     val speedRandom = Random().nextInt(15).toFloat()
                     it.speed = speedRandom
 
-                    var tmpY = 0f
-                    if (it.state == 1)
-                        tmpY = it.y + it.speed
-                    else if (it.state == 2)
-                        tmpY = it.y - it.speed
-                    it.y = tmpY
-                    if (tmpY > it.maxY)
-                        it.state = 2
-                    else if (tmpY < (height / 2).toFloat())
-                        it.state = 1
-
-//                    var tmpX = 0f
-//                    var tmpY = 0f
-//                    when(it.state){
-//                        1 -> {
-//                            tmpX = it.x - it.speed
-//                            tmpY = it.y - it.speed
-//                        }
-//                        2 -> {
-//                            tmpX = it.x + it.speed
-//                            tmpY = it.y - it.speed
-//                        }
-//                        3 -> {
-//                            tmpX = it.x - it.speed
-//                            tmpY = it.y + it.speed
-//                        }
-//                        4 -> {
-//                            tmpX = it.x + it.speed
-//                            tmpY = it.y + it.speed
-//                        }
-//                    }
-//                    it.x = tmpX
-//                    it.y = tmpY
-
+                    it.pos = calcPos(it.pos, it.speed, it.quadrant, it.state)
+                    it.state = checkState(it.pos, it.centerPos,it.minDistance, it.maxDistance)
 
                 }
                 invalidate()
@@ -80,29 +49,62 @@ class ParticleEffectView @JvmOverloads constructor(
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        circlePath.addCircle((width/2).toFloat(), (height/2).toFloat(), (width/4).toFloat(), Path.Direction.CCW)
-        val pathMeasure = PathMeasure(circlePath, false)
-        val pos = FloatArray(2)
-        val tan = FloatArray(2)
+        //  内圆，粒子起点
+        insideCirclePath.addCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 4).toFloat(), Path.Direction.CCW)
+        val insidePathMeasure = PathMeasure(insideCirclePath, false)
+        //  外圆，粒子终点
+        outsideCirclePath.addCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2).toFloat(), Path.Direction.CCW)
+        val outsidePathMeasure = PathMeasure(outsideCirclePath, false)
+
         val random = Random()
         for (index in 0..pointCount) {
-            pathMeasure.getPosTan((index / pointCount.toFloat()) * pathMeasure.length, pos, tan)
-            val x = pos[0] + random.nextInt(5)
-            val y = pos[1] + random.nextInt(5)
-            val bolder = FloatArray(4)
-            bolder[0] = (width/4).toFloat()
-            bolder[1] = (width/4).toFloat()
-            bolder[2] = width.toFloat()
-            bolder[3] = height.toFloat() / 2 + width.toFloat() / 2
+            val startPos = insidePathMeasure.getPosTan(index)
+            val endPos = outsidePathMeasure.getPosTan(index)
+            val randomX = random.nextInt(5)
+            val randomY = random.nextInt(5)
+            startPos[0] += randomX.toFloat()
+            startPos[1] += randomY.toFloat()
+            endPos[0] += randomX.toFloat()
+            endPos[1] += randomY.toFloat()
+            val centerPos = FloatArray(2)
+            centerPos[0] = (width/2).toFloat()
+            centerPos[1] = (height/2).toFloat()
+            val quadrant =  if (startPos[0] < (width / 2).toFloat())
+                if (startPos[1] < (height / 2).toFloat())
+                    1
+                else
+                    2
+            else if (startPos[1] < (height / 2).toFloat())
+                3
+            else
+                4
+            val distanceCTS = abs(
+                    sqrt(
+                            centerPos[0].toDouble().pow(2.0) + centerPos[1].toDouble().pow(2.0)
+                    )      - sqrt(
+                            endPos[0].toDouble().pow(2.0) + endPos[1].toDouble().pow(2.0)
+                    )
+            )
+            val distanceCTE = abs(
+                    sqrt(
+                            centerPos[0].toDouble().pow(2.0) + centerPos[1].toDouble().pow(2.0)
+
+                    )
+                            - sqrt(
+                                    endPos[0].toDouble().pow(2.0) + endPos[1].toDouble().pow(2.0)
+                    )
+            )
+            val pos = startPos.copyOf()
             ppList.add(
                     ParticlePoint(
-                            x,
-                            y,
-                            width.toFloat(),
-                            height.toFloat() / 2 + width.toFloat() / 2,
+                            pos,
+                            centerPos,
+                            distanceCTS,
+                            distanceCTE,
                             5f,
-                           1))
-            Log.e("height/2", "${height / 2}")
+                            0,
+                            quadrant
+                    ))
         }
 
     }
@@ -112,22 +114,92 @@ class ParticleEffectView @JvmOverloads constructor(
         canvas?.drawParticlePoint()
     }
 
+    private fun calcPos(pos: FloatArray, speed: Float, quadrant: Int, state: Int): FloatArray {
+        when(quadrant) {
+            1 -> {
+                if (state == 0) {
+                    pos[0] -= speed
+                    pos[1] -= speed
+                } else {
+                    pos[0] += speed
+                    pos[1] += speed
+                }
+                Log.e("-", "${pos[0]} - ${pos[1]}")
+
+            }
+
+            2 -> {
+                if (state == 0) {
+                    pos[0] -= speed
+                    pos[1] += speed
+                } else {
+                    pos[0] -= speed
+                    pos[1] += speed
+                }
+            }
+
+            3 -> {
+                if (state == 0) {
+                    pos[0] += speed
+                    pos[1] -= speed
+                } else {
+                    pos[0] -= speed
+                    pos[1] += speed
+                }
+            }
+
+            4 -> {
+                if (state == 0) {
+                    pos[0] += speed
+                    pos[1] += speed
+                } else {
+                    pos[0] -= speed
+                    pos[1] -= speed
+                }
+            }
+        }
+        return pos
+    }
+
+    private fun checkState(pos: FloatArray, centerPos: FloatArray,minDistance: Double,  maxDistance: Double): Int {
+        val distance = abs(
+                        sqrt(
+                                pos[0].toDouble().pow(2.0) + pos[1].toDouble().pow(2.0)
+                        ) - sqrt(
+                                centerPos[0].toDouble().pow(2.0) + centerPos[1].toDouble().pow(2.0)
+                        )
+        )
+        var result =  if (distance > maxDistance)
+            1
+        else
+            0
+
+        return result
+    }
+
 
     private fun Canvas.drawParticlePoint() {
+
+        val outsideCircle: Paint = Paint()
+        outsideCircle.color = Color.RED
+        drawPath(outsideCirclePath, outsideCircle)
+        val insideCircle: Paint = Paint()
+        outsideCircle.color = Color.GREEN
+        drawPath(insideCirclePath, insideCircle)
+
         ppList.forEach {
-            drawPoint(it.x, it.y, pointPaint)
+            drawPoint(it.pos[0], it.pos[1], pointPaint)
         }
-//        drawPath(circlePath, pointPaint)
         if (!anim.isRunning) {
             anim.start()
         }
     }
 
-//    private fun checkState(x: Float, y: Float, bolder: FloatArray) : Int{
-//        return if (x > bolder[0] && y < bolder[1])
-//                1
-//        else if (x > bolder[0] && x < bolder[3])
-//            2
-//    }
+    private fun PathMeasure.getPosTan(index: Int): FloatArray {
+        val pos = FloatArray(2)
+        val tan = FloatArray(2)
+        getPosTan((index / pointCount.toFloat()) * length, pos, tan)
+        return pos
+    }
 
 }
